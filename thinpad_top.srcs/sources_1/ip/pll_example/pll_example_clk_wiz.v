fce947b68af5fb4e1c0bef5bce00d2da0b9d1a8f
youyc22 <youyc22@mails.tsinghua.edu.cn>
2024-07-23 01:21:21 +0800

    112

--- thinpad_top.srcs/sources_1/ip/pll_example/pll_example_clk_wiz.v
+++ thinpad_top.srcs/sources_1/ip/pll_example/pll_example_clk_wiz.v
@@ -56,7 +56,7 @@
 //  Output     Output      Phase    Duty Cycle   Pk-to-Pk     Phase
 //   Clock     Freq (MHz)  (degrees)    (%)     Jitter (ps)  Error (ps)
 //----------------------------------------------------------------------------
-// clk_out1__110.00000______0.000______50.0______147.123____162.020
+// clk_out1__111.53846______0.000______50.0______115.289____134.075
 //
 //----------------------------------------------------------------------------
 // Input Clock   Freq (MHz)    Input Jitter (UI)
@@ -123,9 +123,9 @@ wire clk_in2_pll_example;
     .COMPENSATION         ("ZHOLD"),
     .STARTUP_WAIT         ("FALSE"),
     .DIVCLK_DIVIDE        (1),
-    .CLKFBOUT_MULT        (22),
+    .CLKFBOUT_MULT        (29),
     .CLKFBOUT_PHASE       (0.000),
-    .CLKOUT0_DIVIDE       (10),
+    .CLKOUT0_DIVIDE       (13),
     .CLKOUT0_PHASE        (0.000),
     .CLKOUT0_DUTY_CYCLE   (0.500),
     .CLKIN1_PERIOD        (20.000))
