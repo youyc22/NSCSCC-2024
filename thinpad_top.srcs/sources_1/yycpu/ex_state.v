daa8922f28d4a130a7d119b362a1819a51127a44
youyc22 <youyc22@mails.tsinghua.edu.cn>
2024-07-25 00:50:19 +0800

    110.5_new

--- thinpad_top.srcs/sources_1/yycpu/ex_state.v
+++ thinpad_top.srcs/sources_1/yycpu/ex_state.v
@@ -31,12 +31,10 @@ module ex_state(
 );
     // �ڲ��źŶ���
     reg [31:0] logicout, shiftres, arithmeticres;
-    //reg [63:0] mulres;
-    reg [31:0] mulres_32;
+    reg [63:0] mulres;
     wire [31:0] reg2_i_sign;
     wire [31:0] result_sum;
-    //wire [63:0] hilo_temp;
-    wire [31:0] hilo_temp_32;
+    wire [63:0] hilo_temp;
     wire [31:0] opdata1_mult, opdata2_mult;
     //wire overflow_flag;
 
@@ -74,6 +72,20 @@ module ex_state(
 			(aluop_i ==  `SLT_OP) ? (($signed(reg1_i) < $signed(reg2_i)) ? 32'b1 : 32'b0) : `ZeroWord;
 	end
 
+    assign opdata1_mult = (aluop_i == `MUL_OP) ? reg1_i : 32'b0;
+    assign opdata2_mult = (aluop_i == `MUL_OP) ? reg2_i : 32'b0;
+
+    always @(*) begin
+        mulres = (rst == `RstEnable) ? 64'd0 :
+            (aluop_i == `MUL_OP) ? hilo_temp : 64'd0;
+    end
+
+    mult_gen_0 mult_gen_0(
+        .A(opdata1_mult),
+        .B(opdata2_mult),
+        .P(hilo_temp)
+    );
+
     // // �˷�����
     // assign opdata1_mult = ((aluop_i ==  `MUL_OP) && (reg1_i[31] == 1'b1)) ? (~reg1_i + 1) : reg1_i;
     // assign opdata2_mult = ((aluop_i ==  `MUL_OP) && (reg2_i[31] == 1'b1)) ? (~reg2_i + 1) : reg2_i;
@@ -89,20 +101,6 @@ module ex_state(
     //     .mul2(opdata2_mult),
     //     .result(hilo_temp)
     // );
-
-    assign opdata1_mult = (aluop_i == `MUL_OP) ? reg1_i : 32'b0;
-    assign opdata2_mult = (aluop_i == `MUL_OP) ? reg2_i : 32'b0;
-
-    always @(*) begin
-        mulres_32 = (rst == `RstEnable) ? 32'd0 :
-            (aluop_i == `MUL_OP) ? hilo_temp_32 : 32'd0;
-    end
-
-    mult_gen_1 mult_gen_1(
-        .A(opdata1_mult),
-        .B(opdata2_mult),
-        .P(hilo_temp_32)
-    );
 
     // // ������ˮ�߳˷���
     // reg [31:0] mul_op1, mul_op2;
@@ -151,7 +149,7 @@ module ex_state(
              `RES_LOGIC: 		wdata_o = logicout;
              `RES_SHIFT: 		wdata_o = shiftres;
              `RES_ARITHMETIC: 	wdata_o = arithmeticres;
-             `RES_MUL: 			wdata_o = mulres_32;
+             `RES_MUL: 			wdata_o = mulres[31:0];
              `RES_JUMP_BRANCH: 	wdata_o = link_address_i;
             default: 			wdata_o = `ZeroWord;
         endcase
