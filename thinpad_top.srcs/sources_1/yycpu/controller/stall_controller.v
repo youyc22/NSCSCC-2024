09b4654f6f2a5fd39f4a5f83bcaaa405c74dc9b4
youyc22 <youyc22@mails.tsinghua.edu.cn>
2024-07-26 00:59:03 +0800

    aaa

--- thinpad_top.srcs/sources_1/yycpu/controller/stall_controller.v
+++ thinpad_top.srcs/sources_1/yycpu/controller/stall_controller.v
@@ -1,4 +1,3 @@
-`include "defines.v"
 
 module stall_controller(
 	input wire					 rst,
@@ -11,15 +10,15 @@ module stall_controller(
 );
 
 	always @ (*) begin
-		if(rst == `RstEnable) begin
+		if(rst == 1'b1) begin
 			stall <= 6'b000000;
-		end else if(stall_from_icache == `Stop ) begin
+		end else if(stall_from_icache == 1'b1 ) begin
 			stall <= 6'b111111;				
-		end else if(stall_from_mem == `Stop) begin
+		end else if(stall_from_mem == 1'b1) begin
 			stall <= 6'b111111;
-		end else if(stall_from_ex == `Stop) begin
+		end else if(stall_from_ex == 1'b1) begin
 			stall <= 6'b001111;
-		end else if(stall_from_id == `Stop || stall_from_bus == `Stop) begin
+		end else if(stall_from_id == 1'b1 || stall_from_bus == 1'b1) begin
 			stall <= 6'b000111;				
 		end else begin
 			stall <= 6'b000000;
