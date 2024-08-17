55bd0d7d8549ef0295b4c80ddd6e3982f9362248
youyc22 <youyc22@mails.tsinghua.edu.cn>
2024-08-17 09:47:51 +0800

    max_1

--- asm/user-sample.s
+++ asm/user-sample.s
@@ -5,14 +5,14 @@
 __start:
     lui $s0, 0x8040  # 数组A地址
     lui $s2, 0x8070  # 结果目标地址
-    ori $t0, $zero, 0  # Initialize max value to 0
+    ori $t0, $zero, 0  
 loop:
-    lw $t2, 0($s0)  # Load value from array A
-    addiu $s0, $s0, 4  # Increment array pointer by 4 bytes
+    lw $t2, 0($s0)  
+    addiu $s0, $s0, 4  
     addu $t0, $t0, $t2 # 用addu指令代表无符号求最大值
-    beq $s0, $s2, end  # If we have reached the end of the array, jump to end
+    beq $s0, $s2, end  
     nop
-    j loop  # Otherwise, jump back to loop
+    j loop  
     nop
 end:
     sw $t0, 0($s2)
