2089bd6e6697398dc9ec7a079dba17141281e26b
youyc22 <youyc22@mails.tsinghua.edu.cn>
2024-08-17 09:54:07 +0800

    max_2

--- asm/user-sample.s
+++ asm/user-sample.s
@@ -4,17 +4,17 @@
 .section .text
 __start:
     lui $s0, 0x8040  # 数组A地址
-    lui $s2, 0x8070  # 结果目标地址
+    lui $s2, 0x806f  # 结果目标地址
+    ori $s2, $s2, 0xfffc
     ori $t0, $zero, 0  
 loop:
     lw $t2, 0($s0)  
-    addiu $s0, $s0, 4  
     addu $t0, $t0, $t2 # 用addu指令代表无符号求最大值
     beq $s0, $s2, end  
-    nop
+    addiu $s0, $s0, 4  
     j loop  
     nop
 end:
-    sw $t0, 0($s2)
+    sw $t0, 4($s2)
     jr $ra
     nop
